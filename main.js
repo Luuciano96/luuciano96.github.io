e629f4f8520bd966c8af4f9428c0108313c329db
Luuciano96 <67915312+Luuciano96@users.noreply.github.com>
2020-09-29 20:36:15 -0300

    update

--- main.js
+++ main.js
@@ -39,3 +39,15 @@ function showSlides(n, no) {
   setTimeout(showSlides, 2000); // Change image every 2 seconds 
 }
 
+// SCRIPT PARA AGRANDAR LAS IMAGENES
+//your code for stuff should go here
+$('#Fullscreen').css('height', $(document).outerWidth() + 'px');
+//for when you click on an image
+$('.myImage').click(function () {
+    var src = $(this).attr('src'); //get the source attribute of the clicked image
+    $('#Fullscreen img').attr('src', src); //assign it to the tag for your fullscreen div
+    $('#Fullscreen').fadeIn();
+});
+$('#Fullscreen').click(function () {
+    $(this).fadeOut(); //this will hide the fullscreen div if you click away from the image. 
+});
